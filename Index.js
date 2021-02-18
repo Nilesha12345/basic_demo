8b876ff73ae9abe0f4112a3a67d07d3dc5ae3fb8
Nilesh Acharya <nileshacharya9047@gmail.com>
2021-02-18 20:35:45 +0530

    mc9

--- Index.js
+++ Index.js
@@ -1,15 +1,32 @@
 const express = require('express')
 const app = express()
+var createHandler = require('github-webhook-handler')
+var handler = createHandler({ path: '/webhook', secret: 'myhashsecret' })
 const port = 3000
 
-app.post('/webhost', (req, res) => {
-	console.log("Repo Master branchName : ",req.master_branch)
-  console.log("Pusher Name : ",req.pusher.name)
-  console.log("sender name : ",req.login)
+
+app.post('/webhost', (req, res, event) => {
+  handler(req, res, (err)=> {
+    res.statusCode = 404
+    res.end('no such location')
+  })
   res.send(200);
 })
+
+handler.on('push', function (event) {
+  // console.log('Received a push event for %s to %s',
+  //   event.payload.repository.name,
+  //   event.payload.ref)
+  console.log("Repo Name : ",event.payload.repository.name)
+  console.log("Repo full name : ",event.payload.repository.full_name)
+  console.log("Repo owner Name : ",event.payload.repository.owner.name)
+  console.log("Repo URL : ",event.payload.html_url)
+  console.log("Repo Master branchName : ",event.payload.master_branch)
+  console.log("Pusher Name : ",event.payload.pusher.name)
+  console.log("sender name : ",event.payload.login)
+})
+
 app.get('/', (req, res) => {
-    //console.log("helpme")
     res.send('Hii Good Morning!! how may i help you?')
   })
 
