102a6f699c7f47912dd339de4b384ddaf694dfc0
Nilesh Acharya <nileshacharya9047@gmail.com>
2021-02-18 19:52:34 +0530

    minor change1

--- Index.js
+++ Index.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/home', (req, res) => {
-	console.log(req)
-  res.send(req)
+app.post('/webhost', (req, res) => {
+	console.log("Server hit")
+  res.send(200);
 })
 app.get('/', (req, res) => {
     res.send('Hii Good Morning!! how may i help you?')
