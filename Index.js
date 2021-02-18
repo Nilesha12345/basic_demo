6702b80a8df621268dda6c0c2d9c7c6293d25370
Nilesh Acharya <nileshacharya9047@gmail.com>
2021-02-18 20:23:35 +0530

    mc8

--- Index.js
+++ Index.js
@@ -3,11 +3,7 @@ const app = express()
 const port = 3000
 
 app.post('/webhost', (req, res) => {
-	console.log("Repo Name :",req.repository.name)
-  console.log("Repo full name : ",req.repository.full_name)
-  console.log("Repo owner Name : ",req.repository.owner.name)
-  console.log("Repo URL : ",req.html_url)
-  console.log("Repo Master branchName : ",req.master_branch)
+	console.log("Repo Master branchName : ",req.master_branch)
   console.log("Pusher Name : ",req.pusher.name)
   console.log("sender name : ",req.login)
   res.send(200);
